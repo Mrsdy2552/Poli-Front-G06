ad7b1449c8d691bc01a98174687697aa671b02bf
jdvalmart <juanvalencia9411@outlook.com>
2025-04-08 03:47:04 -0500

    actualizacion de componente agregar y creacion del componente eliminar

--- src/app/agregar-modificar/agregar-modificar.component.ts
+++ src/app/agregar-modificar/agregar-modificar.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-agregar-modificar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './agregar-modificar.component.html',
   styleUrl: './agregar-modificar.component.css'
 })
 export class AgregarModificarComponent {
+  producto = {
+    nombre: '',
+    fechaEntrada: '',
+    precioUnitario: 0,
+    categoria: '',
+    stockTotal: 0
+  };
+
+  categorias: string[] = ['Alimentos', 'Electrónica', 'Ropa', 'Hogar', 'Otros'];
+
+  agregarProducto() {
+    console.log('Producto agregado:', this.producto);
+    // Aquí podrías hacer la conexión con un servicio para guardar el producto
+    alert('Producto agregado exitosamente');
+  }
 
 }
