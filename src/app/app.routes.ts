7a74d9c655d169bdbc36deaeefa4b38a78bc8d9c
jdvalmart <juanvalencia9411@outlook.com>
2025-04-08 01:43:53 -0500

    ajustes de routerlink

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'; 
+import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { dashboardRoutes } from './dashboard/dashboard.routes';
 
+
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent }, 
+  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, children: dashboardRoutes },
   { path: '**', redirectTo: 'login' },
 ];
