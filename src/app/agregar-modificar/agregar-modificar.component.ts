3668d16897b5bc17d09791f0387e20357c1ec62f
mrsdy <camelodiego1@hotmail.com>
2025-04-28 10:25:26 -0500

    Implement agregar and eliminar components for product management

--- src/app/agregar-modificar/agregar-modificar.component.ts
+++ src/app/agregar-modificar/agregar-modificar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
   selector: 'app-agregar-modificar',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, MenuComponent],
   templateUrl: './agregar-modificar.component.html',
   styleUrl: './agregar-modificar.component.css'
 })
